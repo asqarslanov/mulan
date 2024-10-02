66e555aadc47f6c8c8286d599beb98525c1f3b47
Asqar Arslanov <asqarslanov@gmail.com>
2024-10-02 17:19:49 +0300

    refactor: add draft template type

--- mulan-parser/src/lib.rs
+++ mulan-parser/src/lib.rs
@@ -1,1 +1,21 @@
+enum DataType {
+    Str(Box<str>),
+    Int(i32),
+    Float(f64),
+}
 
+struct Variable {
+    name: Box<str>,
+    data_type: DataType,
+}
+
+enum Token {
+    Text(Box<str>),
+    Variable(Variable),
+    Extract {
+        name: Box<str>,
+        contents: Box<Token>,
+    },
+}
+
+struct Template(Box<[Token]>);
