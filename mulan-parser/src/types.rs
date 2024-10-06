9008db8de5da714251a15bafea566f56b9b75ca3
Asqar Arslanov <asqarslanov@gmail.com>
2024-10-07 01:25:15 +0300

    refactor(parser): use serde

--- mulan-parser/src/types.rs
+++ mulan-parser/src/types.rs
@@ -1,18 +1,49 @@
 use std::collections::HashMap;
 
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+enum Parameter {
+    Text(Box<str>),
+    Object {
+        name: Box<str>,
+        #[serde(default)]
+        data_type: Type,
+    },
+}
+
+#[derive(Deserialize)]
+struct Options {
+    emoji: bool,
+    character_entity: bool,
+    prettify: bool,
+}
+
+#[derive(Deserialize)]
+struct FullSyntax {
+    #[serde(alias = "params", default)]
+    parameters: Box<[Parameter]>,
+    #[serde(alias = "txt")]
+    text: Box<str>,
+    #[serde(alias = "opts")]
+    options: Options,
+}
+
 struct Lowercase(Box<str>);
 
 struct Identifier(Box<[Lowercase]>);
 
-enum DataType {
-    Str(Box<str>),
-    Int(i32),
-    Float(f64),
+#[derive(Default, Deserialize)]
+enum Type {
+    #[default]
+    Str,
+    Int,
+    Float,
 }
 
 struct Variable {
     name: Identifier,
-    data_type: DataType,
+    data_type: Type,
 }
 
 enum Token {
