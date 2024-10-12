c4b663b70853f1c4c23c6dba23d7d1c7b394c395
Asqar Arslanov <asqarslanov@gmail.com>
2024-10-12 12:46:02 +0300

    refactor(parser): remove unnecessary err propagations

--- mulan-parser/src/lib.rs
+++ mulan-parser/src/lib.rs
@@ -9,5 +9,5 @@ pub fn parse_translation(name: &str) -> Result<output::Translation> {
     let path = Path::new("locales").join(name).join("locale.json5");
     let raw = std::fs::read_to_string(path)?;
     let parsed: input::Translation = json5::from_str(&raw)?;
-    Ok(parsed.try_into()?)
+    parsed.try_into()
 }
