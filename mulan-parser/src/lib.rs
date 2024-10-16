be458339550ac0c629a4cd4d425440f7f8c30738
Asqar Arslanov <asqarslanov@gmail.com>
2024-10-16 09:54:44 +0300

    refactor(parser): require passing config

--- mulan-parser/src/lib.rs
+++ mulan-parser/src/lib.rs
@@ -1,11 +1,12 @@
 use std::path::Path;
 
 use anyhow::Result;
+use mulan_config::Config;
 
 mod input;
 mod output;
 
-pub fn parse_translation(name: &str) -> Result<output::Translation> {
+pub fn parse_translation(name: &str, _config: &Config) -> Result<output::Translation> {
     let entry_point = Path::new("locale").with_extension("json5");
 
     let path = Path::new("locales").join(name).join(entry_point);
