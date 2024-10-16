c5978e965b0f274d2a85553ecea0774925f2ea7d
Asqar Arslanov <asqarslanov@gmail.com>
2024-10-16 09:46:36 +0300

    refactor(parser): extract reading files to its own fn

--- mulan-parser/src/lib.rs
+++ mulan-parser/src/lib.rs
@@ -6,8 +6,15 @@ mod input;
 mod output;
 
 pub fn parse_translation(name: &str) -> Result<output::Translation> {
-    let path = Path::new("locales").join(name).join("locale.json5");
-    let raw = std::fs::read_to_string(path)?;
-    let parsed: input::Translation = json5::from_str(&raw)?;
+    let entry_point = Path::new("locale").with_extension("json5");
+
+    let path = Path::new("locales").join(name).join(entry_point);
+    let parsed = load_file(path)?;
     parsed.try_into()
+}
+
+pub(crate) fn load_file(path: impl AsRef<Path>) -> Result<input::Translation> {
+    let raw = std::fs::read_to_string(path)?;
+    let parsed = json5::from_str(&raw)?;
+    Ok(parsed)
 }
