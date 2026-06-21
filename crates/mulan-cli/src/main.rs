11e508181290b53a822834675d43ee6bd2fe563d
Asqar Arslanov <asqarslanov@gmail.com>
2026-06-22 00:32:14 +0500

    feat(cli): write basic cli

--- crates/mulan-cli/src/main.rs
+++ crates/mulan-cli/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    todo!();
+use std::process::ExitCode;
+
+use clap::crate_description;
+
+mod cmd_gen;
+
+fn main() -> miette::Result<ExitCode> {
+    let cli = <self::Cli as clap::Parser>::parse();
+    match cli.command {
+        Command::Gen(args) => args.execute(),
+    }
+}
+
+#[derive(clap::Parser)]
+#[command(about = crate_description!(), version)]
+struct Cli {
+    #[command(subcommand)]
+    command: self::Command,
+}
+
+#[derive(clap::Subcommand)]
+enum Command {
+    /// Generate i18n bindings for your targets
+    Gen(self::cmd_gen::Args),
 }
