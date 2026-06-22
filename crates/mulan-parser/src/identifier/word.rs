5629700b82ac55ae8592a32b5ae405e9cad7fb8f
Asqar Arslanov <asqarslanov@gmail.com>
2026-06-23 00:15:00 +0500

    document word

--- crates/mulan-parser/src/identifier/word.rs
+++ crates/mulan-parser/src/identifier/word.rs
@@ -2,15 +2,10 @@
 
 use compact_str::CompactString;
 
-/// ...
+/// A part of an [`Identifier`](crate::identifier::Identifier).
 ///
-/// ## Valid Examples
-///
-/// ...
-///
-/// ## Invalid Examples
-///
-/// ...
+/// For example, the identifier `student-bs23-id006` consists of three
+/// [`Word`]s: `student`, `bs23`, and `id006`.
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Word {
     pub(super) inner: CompactString,
