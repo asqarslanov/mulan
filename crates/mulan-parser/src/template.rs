b5d19a4b4f6381cb0c0249193cc6f4455931e6b2
Asqar Arslanov <asqarslanov@gmail.com>
2026-06-23 02:08:29 +0500

    part parser

--- crates/mulan-parser/src/template.rs
+++ crates/mulan-parser/src/template.rs
@@ -14,15 +14,44 @@ pub struct Parameter {
 /// ...
 #[derive(Debug)]
 pub struct Template {
-    parts: SmallVec<[TemlpatePart; 1]>,
+    parts: SmallVec<[TemplatePart; 1]>,
 }
 
 /// ...
 #[derive(Debug)]
-enum TemlpatePart {
+enum TemplatePart {
     /// ...
     Text(CompactString),
 
     /// ...
     Placeholder(Parameter),
+}
+
+mod parser {
+    use chumsky::prelude::*;
+    use compact_str::CompactString;
+
+    use super::{Parameter, TemplatePart};
+    use crate::identifier::Identifier;
+
+    impl TemplatePart {
+        #[must_use]
+        pub fn chumsky_parser<'src>()
+        -> impl Parser<'src, &'src str, Self, extra::Err<Rich<'src, char>>> {
+            let text = {
+                choice((just("{{").to('{'), just("}}").to('}'), none_of("{}")))
+                    .repeated()
+                    .at_least(1)
+                    .collect::<String>()
+                    .map(|it| Self::Text(CompactString::new(it)))
+            };
+            let expr = {
+                Identifier::chumsky_parser()
+                    .padded()
+                    .delimited_by(just('{'), just('}'))
+                    .map(|name| Self::Placeholder(Parameter { name }))
+            };
+            choice((text, expr))
+        }
+    }
 }
