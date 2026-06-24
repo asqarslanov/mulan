2c7dd9522b66e7df74793a9379c24c2e937bef2b
Asqar Arslanov <asqarslanov@gmail.com>
2026-06-24 19:38:27 +0500

    document def

--- crates/mulan-parser/src/schemas/input.rs
+++ crates/mulan-parser/src/schemas/input.rs
@@ -15,10 +15,29 @@ pub struct Input {
     pub other_locales: HashMap<Language, Definition>,
 }
 
-/// ...
+/// A single-language definition of a locale read from a locale file
+/// (e.g., `locales/en-US/locale.yaml`).
+///
+/// This structure uses basic data types and is constructed
+/// with [`serde::Deserialize`]. Later, a more strongly typed tree
+/// can be constructed from a collection of [`Definition`]s.
+///
+/// ## Example Definition
+///
+/// ```yaml
+/// app-name: "Mulan"
+/// greeting: "Hello, {name}!"
+/// namespace-foo:
+///   lorem-upsum: "Dolor sit amet"
+/// ```
 #[derive(Debug, Deserialize)]
 pub struct Definition {
-    /// ...
+    /// A locale definition is ultimately a tree of nested namespaces
+    /// (see [`RawNamespace`]).
+    ///
+    /// A definition is considered flat if it only has one outermost
+    /// namespace---the **root namespace**. However, the root namespace
+    /// is always present, even if the locale definition is empty.
     #[serde(flatten)]
     root: RawNamespace,
 }
