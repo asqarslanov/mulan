bc960cf164657c8511115b8539c4b9d5c6db39e1
Asqar Arslanov <asqarslanov@gmail.com>
2026-06-25 00:50:54 +0500

    rename vars

--- crates/mulan-parser/src/schemas.rs
+++ crates/mulan-parser/src/schemas.rs
@@ -20,12 +20,12 @@ enum ReadError {
 
 impl Input {
     fn read() -> Result<Self, ReadError> {
-        let path = PathBuf::from("locales/en-US/locale.yaml");
-        let file_contents = fs::read_to_string(&path).map_err(|_| ReadError::Fs(path))?;
-        let definition: Definition =
+        let file_path = PathBuf::from("locales/en-US/locale.yaml");
+        let file_contents = fs::read_to_string(&file_path).map_err(|_| ReadError::Fs(file_path))?;
+        let locale_definition: Definition =
             serde_saphyr::from_str(&file_contents).map_err(ReadError::Yaml)?;
         let result = Input {
-            locales: iter::once((Language::EnUs, definition)).collect(),
+            locales: iter::once((Language::EnUs, locale_definition)).collect(),
         };
         Ok(result)
     }
