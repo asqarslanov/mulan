86265e86a8379227d870d78b3c98741dbf9ace3a
Asqar Arslanov <asqarslanov@gmail.com>
2026-06-24 18:44:37 +0500

    document schemas

--- crates/mulan-parser/src/schemas.rs
+++ crates/mulan-parser/src/schemas.rs
@@ -1,3 +1,6 @@
-//! ...
+//! Defines structures this crate operates on and operations on them.
+//!
+//! Most notably, [`mulan_parser::Input`](self::input::Input),
+//! and _`mulan_parser::Output`_.
 
 mod input;
