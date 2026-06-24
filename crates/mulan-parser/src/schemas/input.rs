6740bfc730bf2831665c490a29631b171c73f319
Asqar Arslanov <asqarslanov@gmail.com>
2026-06-24 11:59:48 +0500

    define schemas

---
+++ crates/mulan-parser/src/schemas/input.rs
@@ -0,0 +1,29 @@
+use compact_str::CompactString;
+use foldhash::HashMap;
+use mulan_config::Language;
+use serde::Deserialize;
+
+#[derive(Debug)]
+pub struct Input {
+    pub default_locale: Definition,
+    pub other_locales: HashMap<Language, Definition>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Definition {
+    #[serde(flatten)]
+    root: RawNamespace,
+}
+
+#[derive(Debug, Deserialize)]
+struct RawNamespace {
+    #[serde(flatten)]
+    map: HashMap<CompactString, RawNode>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(untagged)]
+enum RawNode {
+    Message(CompactString),
+    Namespace(RawNamespace),
+}
