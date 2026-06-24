78fc09426d2c66bad2457feec8471fcf4e7cb166
Asqar Arslanov <asqarslanov@gmail.com>
2026-06-24 21:24:59 +0500

    use

--- crates/mulan-parser/src/schemas.rs
+++ crates/mulan-parser/src/schemas.rs
@@ -1,6 +1,7 @@
 //! Defines structures this crate operates on and operations on them.
 //!
-//! Most notably, [`mulan_parser::Input`](self::input::Input),
-//! and [`mulan_parser::Output`](self::output::Output).
+//! Most notably, [`Input`] and [`Output`].
+
+use self::input::Input;
 
 mod input;
