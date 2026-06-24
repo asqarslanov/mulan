f4251fcbd8ac516e3ea6190b72ee5cc707c93fcb
Asqar Arslanov <asqarslanov@gmail.com>
2026-06-24 13:48:37 +0500

    ...

--- crates/mulan-parser/src/schemas/input.rs
+++ crates/mulan-parser/src/schemas/input.rs
@@ -5,27 +5,39 @@ use foldhash::HashMap;
 use mulan_config::Language;
 use serde::Deserialize;
 
+/// ...
 #[derive(Debug)]
 pub struct Input {
+    /// ...
     pub default_locale: Definition,
+
+    /// ...
     pub other_locales: HashMap<Language, Definition>,
 }
 
+/// ...
 #[derive(Debug, Deserialize)]
 pub struct Definition {
+    /// ...
     #[serde(flatten)]
     root: RawNamespace,
 }
 
+/// ...
 #[derive(Debug, Deserialize)]
 pub struct RawNamespace {
+    /// ...
     #[serde(flatten)]
     map: HashMap<CompactString, RawNode>,
 }
 
+/// ...
 #[derive(Debug, Deserialize)]
 #[serde(untagged)]
 pub enum RawNode {
+    /// ...
     Message(CompactString),
+
+    /// ...
     Namespace(RawNamespace),
 }
