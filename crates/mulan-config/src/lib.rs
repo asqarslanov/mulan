887470576a6a4605121538e70e530744056e44dd
Asqar Arslanov <asqarslanov@gmail.com>
2026-06-24 13:41:17 +0500

    document lang

--- crates/mulan-config/src/lib.rs
+++ crates/mulan-config/src/lib.rs
@@ -1,6 +1,6 @@
-/// ...
+/// An [IETF language tag](https://en.wikipedia.org/wiki/IETF_language_tag) used as a locale name.
 #[derive(Debug)]
 pub enum Language {
-    /// ...
+    /// English (United States)
     EnUs,
 }
