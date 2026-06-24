f83a9f7ceefcbb3a98a558d6b8d98bdbccb28296
Asqar Arslanov <asqarslanov@gmail.com>
2026-06-24 21:48:55 +0500

    impl read

--- crates/mulan-config/src/lib.rs
+++ crates/mulan-config/src/lib.rs
@@ -1,6 +1,6 @@
 /// A [BCP 47 language tag](https://en.wikipedia.org/wiki/IETF_language_tag)
 /// used as a locale name (e.g., `en-US` or `ru-RU`).
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Hash)]
 pub enum Language {
     /// English (United States)
     EnUs,
--- crates/mulan-parser/src/schemas.rs
+++ crates/mulan-parser/src/schemas.rs
@@ -2,15 +2,32 @@
 //!
 //! Most notably, [`Input`] and [`Output`].
 
-use self::input::Input;
+use std::fs::File;
+use std::io::BufReader;
+use std::iter;
+use std::path::PathBuf;
+
+use mulan_config::Language;
+
+use self::input::{Definition, Input};
 
 mod input;
 
 #[derive(Debug)]
-enum ReadError {}
+enum ReadError {
+    Fs(PathBuf),
+    Yaml(serde_saphyr::Error),
+}
 
 impl Input {
     fn read() -> Result<Self, ReadError> {
-        todo!();
+        let path = PathBuf::from("locales/en-US/locale.yaml");
+        let file = File::open(&path).map_err(|_| ReadError::Fs(path))?;
+        let en_us: Definition =
+            serde_saphyr::from_reader(BufReader::new(file)).map_err(ReadError::Yaml)?;
+        let result = Input {
+            locales: iter::once((Language::EnUs, en_us)).collect(),
+        };
+        Ok(result)
     }
 }
