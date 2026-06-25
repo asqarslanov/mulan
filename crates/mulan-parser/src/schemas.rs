9545cf48a7ce63b13c799b54f00706e2bf29a20f
Asqar Arslanov <asqarslanov@gmail.com>
2026-06-25 19:52:00 +0500

    impl definition

--- crates/mulan-parser/src/schemas.rs
+++ crates/mulan-parser/src/schemas.rs
@@ -19,14 +19,17 @@ enum ReadError {
 }
 
 impl Input {
-    fn read() -> Result<Self, ReadError> {
-        let file_path = PathBuf::from("locales/en-US/locale.yaml");
+    fn read(file_path: PathBuf) -> Result<Self, ReadError> {
+        let en_us_path = PathBuf::from("locales/en-US/locale.yaml");
+        let en_us_definition = Definition::read(en_us_path)?;
+        let locales = iter::once((Language::EnUs, en_us_definition)).collect();
+        Ok(Input { locales })
+    }
+}
+
+impl Definition {
+    fn read(file_path: PathBuf) -> Result<Self, ReadError> {
         let file_contents = fs::read_to_string(&file_path).map_err(|_| ReadError::Fs(file_path))?;
-        let locale_definition: Definition =
-            serde_saphyr::from_str(&file_contents).map_err(ReadError::Yaml)?;
-        let result = Input {
-            locales: iter::once((Language::EnUs, locale_definition)).collect(),
-        };
-        Ok(result)
+        serde_saphyr::from_str(&file_contents).map_err(ReadError::Yaml)
     }
 }
