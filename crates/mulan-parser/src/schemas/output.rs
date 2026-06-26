7dca11aa5690f40272de6b32dc88056100ade7e9
Asqar Arslanov <asqarslanov@gmail.com>
2026-06-26 20:02:51 +0500

    ...

--- crates/mulan-parser/src/schemas/output.rs
+++ crates/mulan-parser/src/schemas/output.rs
@@ -1,3 +1,5 @@
+//! ...
+
 use std::collections::BTreeMap;
 
 use mulan_config::Language;
@@ -5,29 +7,43 @@ use mulan_config::Language;
 use crate::identifier::Identifier;
 use crate::template::Template;
 
+/// ...
 #[derive(Debug)]
 pub struct Output {
+    /// ...
     root: Namespace,
 }
 
+/// ...
 #[derive(Debug)]
 pub struct Namespace {
+    /// ...
     map: BTreeMap<Key, Node>,
 }
 
+/// ...
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Key {
+    /// ...
     value: Identifier,
 }
 
+/// ...
 #[derive(Debug)]
 pub enum Node {
+    /// ...
     Message(Translations),
+
+    /// ...
     Namespace(Namespace),
 }
 
+/// ...
 #[derive(Debug)]
 pub struct Translations {
+    /// ...
     default: Template,
+
+    /// ...
     others: BTreeMap<Language, Template>,
 }
