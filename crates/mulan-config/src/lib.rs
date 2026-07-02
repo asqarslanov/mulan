5c8d0a0595c4a503c9d2f11850fc8a6735240dac
Asqar Arslanov <asqarslanov@gmail.com>
2026-07-02 18:55:43 +0500

    no mitsein

--- crates/mulan-config/src/lib.rs
+++ crates/mulan-config/src/lib.rs
@@ -1,21 +1,28 @@
 //! See [`Config`].
 
-use mitsein::btree_set1::BTreeSet1;
+use std::collections::BTreeSet;
+
+use serde::Deserialize;
+use serde_with::{SetPreventDuplicates, serde_as};
 
 /// A [BCP 47 language tag](https://en.wikipedia.org/wiki/IETF_language_tag)
 /// used as a locale name (e.g., `en-US` or `ru-RU`).
-#[derive(Debug, PartialEq, Eq, Hash)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Deserialize)]
 pub enum Language {
     /// English (United States)
+    #[serde(rename = "en-US")]
     EnUs,
 }
 
 /// ...
-#[derive(Debug)]
-pub struct Config {
+#[serde_as]
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+struct Input {
     /// ...
-    pub default_locale: Language,
+    #[serde_as(as = "SetPreventDuplicates<_>")]
+    pub locales: BTreeSet<Language>,
 
     /// ...
-    pub locales: BTreeSet1<Language>,
+    pub default_locale: Language,
 }
