d8aeccd46e634e9b001657c52dbaba3eed688ea5
Asqar Arslanov <asqarslanov@gmail.com>
2026-07-02 18:05:38 +0500

    basic schema

--- crates/mulan-config/src/lib.rs
+++ crates/mulan-config/src/lib.rs
@@ -1,7 +1,21 @@
+//! See [`Config`].
+
+use std::collections::BTreeSet;
+
 /// A [BCP 47 language tag](https://en.wikipedia.org/wiki/IETF_language_tag)
 /// used as a locale name (e.g., `en-US` or `ru-RU`).
 #[derive(Debug, PartialEq, Eq, Hash)]
 pub enum Language {
     /// English (United States)
     EnUs,
+}
+
+/// ...
+#[derive(Debug)]
+pub struct Config {
+    /// ...
+    pub default_locale: Language,
+
+    /// ...
+    pub locales: BTreeSet<Language>,
 }
