7bec7e09114a73f93f46f8e861093a5a58701069
Asqar Arslanov <asqarslanov@gmail.com>
2026-07-04 00:48:42 +0500

    remove transform

--- crates/mulan-parser/src/schemas.rs
+++ crates/mulan-parser/src/schemas.rs
@@ -3,7 +3,23 @@
 //! Most notably, [`Input`] and [`Output`].
 
 pub use self::input::Input; // TODO: use it privately
+use self::output::Output;
 
 mod input;
 pub mod output;
-mod transform;
+
+#[derive(Debug)]
+enum TransformError {
+    NoDefaultLocale,
+}
+
+fn transform(mut input: Input, config: &mulan_config::Config) -> Result<Output, TransformError> {
+    let default_locale = {
+        input
+            .locales
+            .remove(&config.default_locale)
+            .ok_or(TransformError::NoDefaultLocale)?
+    };
+    // for (key, node) in default_locale.root.map {}
+    Ok(Output { root })
+}
