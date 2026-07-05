525d926f850a1c7aeef3f9621f0b3f06b6fdf16a
Asqar Arslanov <asqarslanov@gmail.com>
2026-07-05 12:12:36 +0500

    define ChumskyParser

--- crates/mulan-parser/src/lib.rs
+++ crates/mulan-parser/src/lib.rs
@@ -1,3 +1,5 @@
+use trait_set::trait_set;
+
 pub use self::schemas::Input; // TODO: remove this export
 pub use self::schemas::output::{Key, Namespace, Node, Output, Translations};
 pub use self::template::Template;
@@ -5,3 +7,13 @@ pub use self::template::Template;
 mod identifier;
 mod schemas;
 mod template;
+
+trait_set! {
+    /// ...
+    trait ChumskyParser<T> = for<'src> chumsky::Parser<
+            'src,
+            &'src str,
+            T,
+            chumsky::extra::Err<chumsky::error::Rich<'src, char>>,
+        >;
+}
--- crates/mulan-parser/src/schemas.rs
+++ crates/mulan-parser/src/schemas.rs
@@ -11,7 +11,7 @@ use mulan_config::Language;
 pub use self::input::Input; // TODO: use it privately
 use self::input::RawNode;
 use self::output::Output;
-use crate::Template;
+use crate::{ChumskyParser, Template};
 
 mod input;
 pub mod output;
@@ -44,12 +44,7 @@ fn transform(mut input: Input, config: &mulan_config::Config) -> Result<Output, 
 fn translations(
     input: &Input,
     path: &SmallVec1<[CompactString; 2]>,
-    template_parser: &impl for<'src> chumsky::Parser<
-        'src,
-        &'src str,
-        Template,
-        chumsky::extra::Err<chumsky::error::Rich<'src, char>>,
-    >,
+    template_parser: &impl ChumskyParser<Template>,
     config: &mulan_config::Config,
 ) -> Result<BTreeMap<Language, Template>, ()> {
     config
