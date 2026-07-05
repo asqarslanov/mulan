daadf20ae70782ea8a41049f71c09f5ad7af7f16
Asqar Arslanov <asqarslanov@gmail.com>
2026-07-05 17:31:33 +0500

    .

--- crates/mulan-parser/src/schemas.rs
+++ crates/mulan-parser/src/schemas.rs
@@ -52,9 +52,7 @@ fn translations<'src>(
         .locales_except_default()
         .map(|lang| {
             let definition = input.locales.get(&lang).ok_or((/* UndefinedLocale */))?;
-            let node = {
-                definition.at(path).ok_or((/* SubkeyNotFound */))?
-            };
+            let node = definition.at(path).ok_or((/* SubkeyNotFound */))?;
             let raw_template = match node {
                 RawNode::Message(template) => template,
                 RawNode::Namespace(_) => return Err(()), // ...
