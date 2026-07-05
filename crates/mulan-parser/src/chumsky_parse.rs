49a51baa51816762db0cc4b8bb199ee1e1bdf16e
Asqar Arslanov <asqarslanov@gmail.com>
2026-07-05 16:32:53 +0500

    .

--- crates/mulan-parser/src/chumsky_parse.rs
+++ crates/mulan-parser/src/chumsky_parse.rs
@@ -33,19 +33,20 @@ pub trait ChumskyParser<'src, Out>:
 {
     /// ...
     fn mulan_parse(&self, source: &'src str) -> Result<Out, ChumskyAllErrors> {
-        let result = self.parse(source).into_result();
-        result.map_err(|errors| ChumskyAllErrors {
-            source: source.into(),
-            errors: errors
-                .into_iter()
-                .try_into_iter1()
-                .expect("if parsing failed, there should at least be one error")
-                .map(|err| ChumskySingleError {
-                    message: err.to_compact_string(),
-                    span: err.span().into_range().into(),
-                })
-                .collect1(),
-        })
+        self.parse(source)
+            .into_result()
+            .map_err(|errors| ChumskyAllErrors {
+                source: source.into(),
+                errors: errors
+                    .into_iter()
+                    .try_into_iter1()
+                    .expect("if parsing failed, there should at least be one error")
+                    .map(|err| ChumskySingleError {
+                        message: err.to_compact_string(),
+                        span: err.span().into_range().into(),
+                    })
+                    .collect1(),
+            })
     }
 }
 
