1eb4368e8c49af20bb90c0e2fb3ef8afcdce0836
Asqar Arslanov <asqarslanov@gmail.com>
2026-07-05 23:44:02 +0500

    .

--- crates/mulan-parser/src/lib.rs
+++ crates/mulan-parser/src/lib.rs
@@ -1,6 +1,6 @@
 pub use self::schemas::Input; // TODO: remove this export
 pub use self::schemas::output::{Key, Namespace, Node, Output, Subkey, Translations};
-pub use self::template::Template;
+pub use self::template::{Parameter, Template, TemplatePart};
 
 mod chumsky_parse;
 mod identifier;
