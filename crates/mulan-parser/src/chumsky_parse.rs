36e4285c7fc3b9f3d39ffe42dce319c6b72bd9df
Asqar Arslanov <asqarslanov@gmail.com>
2026-07-05 13:59:09 +0500

    mulan_parse

--- crates/mulan-parser/src/chumsky_parse.rs
+++ crates/mulan-parser/src/chumsky_parse.rs
@@ -1,9 +1,55 @@
 //! ...
 
-use chumsky::prelude::*;
-use trait_set::trait_set;
+use std::range::Range;
 
-trait_set! {
+use chumsky::prelude::*;
+use compact_str::{CompactString, ToCompactString as _};
+use mitsein::iter1::IteratorExt as _;
+use mitsein::small_vec1::SmallVec1;
+
+/// ...
+#[derive(Debug)]
+pub struct ChumskyAllErrors {
     /// ...
-    pub trait ChumskyParser<'src, T> = Parser<'src, &'src str, T, extra::Err<Rich<'src, char>>>;
+    pub source: CompactString,
+
+    /// ...
+    pub errors: SmallVec1<[ChumskySingleError; 1]>,
+}
+
+/// ...
+#[derive(Debug)]
+pub struct ChumskySingleError {
+    /// ...
+    pub message: CompactString,
+
+    /// ...
+    pub span: Range<usize>,
+}
+
+/// ...
+pub trait ChumskyParser<'src, T>: Parser<'src, &'src str, T, extra::Err<Rich<'src, char>>> {
+    /// ...
+    fn mulan_parse(&self, source: &'src str) -> Result<T, ChumskyAllErrors> {
+        let result = self.parse(source).into_result();
+        result.map_err(|errors| ChumskyAllErrors {
+            source: source.into(),
+            errors: {
+                errors
+                    .into_iter()
+                    .try_into_iter1()
+                    .expect("if parsing failed, there should at least be one error")
+                    .map(|err| ChumskySingleError {
+                        message: err.to_compact_string(),
+                        span: err.span().into_range().into(),
+                    })
+                    .collect1()
+            },
+        })
+    }
+}
+
+impl<'src, S, T> ChumskyParser<'src, S> for T where
+    T: Parser<'src, &'src str, S, extra::Err<Rich<'src, char>>>
+{
 }
