509869b3dde230979daac480deabd931f6f28152
Asqar Arslanov <asqarslanov@gmail.com>
2026-07-06 01:00:09 +0500

    wow

--- crates/mulan-parser/src/schemas.rs
+++ crates/mulan-parser/src/schemas.rs
@@ -7,21 +7,37 @@ use std::collections::{BTreeMap, BTreeSet};
 use compact_str::CompactString;
 use mitsein::small_vec1::SmallVec1;
 use mulan_config::Language;
+use smallvec::SmallVec;
 
 pub use self::input::Input; // TODO: use it privately
-use self::input::{DefinitionAtError, RawNode};
+use self::input::{DefinitionAtError, RawNamespace, RawNode};
 use self::output::Output;
-use crate::Template;
-use crate::chumsky_parse::ChumskyParser;
+use crate::chumsky_parse::{ChumskyAllErrors, ChumskyParser};
+use crate::{Namespace, Node, Template};
 
 mod input;
 pub mod output;
 
-/// ...
+/// Errors of [`transform`].
 #[derive(Debug)]
 enum TransformError {
     /// ...
-    NoDefaultLocale,
+    LocaleNotFound(Language),
+
+    /// ...
+    InvalidSubkey {
+        locale: Language,
+
+        /// ...
+        path: SmallVec1<[CompactString; 1]>,
+    },
+
+    /// ...
+    InvalidTemplate {
+        locale: Language,
+        path: SmallVec1<[CompactString; 1]>,
+        errors: ChumskyAllErrors,
+    },
 }
 
 /// ...
@@ -34,18 +50,59 @@ fn transform(
         input
             .locales
             .remove(&config.default_locale)
-            .ok_or(TransformError::NoDefaultLocale)?
+            .ok_or(TransformError::LocaleNotFound(config.default_locale))?
     };
-    for (subkey, node) in default_locale.root.map {
-        match node {
-            RawNode::Message(template_raw) => {
-                let template = template_parser.mulan_parse(&template_raw);
-                todo!();
-            }
-            RawNode::Namespace(namespace_raw) => todo!(),
+    let namespace = traverse_namespace(
+        &mut SmallVec::new(),
+        default_locale.root,
+        template_parser,
+        config,
+    )?;
+    Ok(Output { root: todo!() })
+}
+
+/// ...
+fn traverse_namespace(
+    key: &mut SmallVec<[CompactString; 1]>,
+    namespace: RawNamespace,
+    template_parser: &impl for<'src> ChumskyParser<'src, Template>,
+    config: &mulan_config::Config,
+) -> Result<Namespace, TransformError> {
+    for (subkey, raw_node) in namespace.map {
+        key.push(subkey);
+        let handle_node_result = handle_node(raw_node, key, template_parser, config);
+        key.pop();
+        let node = handle_node_result?;
+    }
+    todo!();
+}
+
+/// ...
+fn handle_node(
+    node: RawNode,
+    key: &mut SmallVec<[CompactString; 1]>,
+    template_parser: &impl for<'src> ChumskyParser<'src, Template>,
+    config: &mulan_config::Config,
+) -> Result<Node, TransformError> {
+    match node {
+        RawNode::Message(template_raw) => {
+            let a = template_parser
+                .mulan_parse(&template_raw)
+                .map_err(|errors| TransformError::InvalidTemplate {
+                    locale: config.default_locale,
+                    path: {
+                        key.to_owned()
+                            .try_into()
+                            .expect("`subkey` pushed at every iteration")
+                    },
+                    errors,
+                })?;
+        }
+        RawNode::Namespace(inner_namespace) => {
+            traverse_namespace(key, inner_namespace, template_parser, config)?;
         }
     }
-    Ok(Output { root: todo!() })
+    todo!();
 }
 
 /// ...
