db9d8914bf406c8ef3dc0d76d9453510267ce92b
Asqar Arslanov <asqarslanov@gmail.com>
2026-07-07 01:40:52 +0500

    .

--- crates/mulan-parser/src/lib.rs
+++ crates/mulan-parser/src/lib.rs
@@ -1,5 +1,6 @@
 //! ...
 
+use self::identifier::{Identifier, Word};
 use self::schemas::input::Input;
 pub use self::schemas::output::{Key, Namespace, Node, Output, Subkey, Translations};
 pub use self::template::{Parameter, Template, TemplatePart};
@@ -14,8 +15,8 @@ pub fn read_and_parse(
     config: &mulan_config::Config,
 ) -> Result<Output, self::schemas::TransformError> {
     let input = Input::read().unwrap();
-    let word_parser = crate::identifier::Word::chumsky_parser();
-    let ident_parser = crate::identifier::Identifier::chumsky_parser(&word_parser);
+    let word_parser = Word::chumsky_parser();
+    let ident_parser = Identifier::chumsky_parser(&word_parser);
     let subkey_parser = Subkey::chumsky_parser(&ident_parser);
     let param_parser = Parameter::chumsky_parser(&ident_parser);
     let template_part_parser = TemplatePart::chumsky_parser(&param_parser);
