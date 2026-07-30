7dd9e6f9569533a715934fa49ebc08557b2c132b
Asqar Arslanov <asqarslanov@gmail.com>
2026-07-31 00:02:32 +0500

    .

--- crates/mulan-config/src/lib.rs
+++ crates/mulan-config/src/lib.rs
@@ -1,21 +1,16 @@
 //! See [`Config`] and [`Config::locate_and_read`].
 
 use std::collections::BTreeSet;
-use std::ffi::OsStr;
-use std::path::PathBuf;
-use std::{env, io};
 
 use figment2::Figment;
 use figment2::providers::{Format as _, Toml};
-use itertools::Itertools as _;
-use mitsein::btree_set1::BTreeSet1;
-use mitsein::iter1::IteratorExt as _;
-use relative_path::RelativePathBuf;
 use serde_with::{SetPreventDuplicates, serde_as};
 
 pub use self::language::Language;
+pub use self::meta::{ConfigMeta, MetaError};
 
 mod language;
+mod meta;
 
 /// # Mulan Config
 ///
@@ -95,81 +90,6 @@ pub enum ConfigError {
 
     /// ...
     Meta(MetaError),
-}
-
-/// See [`Config::meta`].
-#[derive(Debug, Default, PartialEq, Eq)]
-pub struct ConfigMeta {
-    /// See [`std::env::current_dir`].
-    pub current_dir: PathBuf,
-
-    /// The path of the project root directory Mulan is operating on,
-    /// relative to [`Self::current_dir`].
-    pub root_dir: RelativePathBuf,
-}
-
-/// ...
-#[derive(Debug)]
-pub enum MetaError {
-    /// Failed to call [`std::env::current_dir`].
-    CurrentDir(io::Error),
-
-    /// Unable to locate a config file anywhere.
-    SourceNotFound,
-
-    /// Multiple config files found (only one is permitted).
-    AmbiguousSource(BTreeSet1<RelativePathBuf>),
-}
-
-impl ConfigMeta {
-    fn new(figment: &Figment) -> Result<Self, MetaError> {
-        let current_dir = env::current_dir().map_err(MetaError::CurrentDir)?;
-        let (root_dir, _config_file) = {
-            figment
-                .metadata()
-                .filter_map(|metadata| {
-                    let source_absolute = {
-                        metadata
-                            .source
-                            .as_ref()
-                            .expect("all sources are predetermined")
-                            .file_path()
-                            .expect("config is only read from a file")
-                    };
-                    if source_absolute.is_relative() {
-                        return None;
-                    }
-                    let (root_dir_absolute, config_file) = {
-                        source_absolute
-                            .parent()
-                            .zip(source_absolute.file_name().and_then(OsStr::to_str))
-                            .expect("config source should point to a file")
-                    };
-                    let root_dir_raw = pathdiff::diff_paths(root_dir_absolute, &current_dir)
-                        .expect("current_dir can be subtracted from config source");
-                    let root_dir = RelativePathBuf::from_path(root_dir_raw)
-                        .expect("pathdiff::diff_paths returns a relative path");
-                    Some((root_dir, config_file))
-                })
-                .exactly_one()
-                .map_err(|locations| {
-                    locations
-                        .try_into_iter1()
-                        .map_or(MetaError::SourceNotFound, |sources_raw| {
-                            let sources = {
-                                sources_raw
-                                    .map(|(root_dir, config_file)| root_dir.join(config_file))
-                                    .collect1()
-                            };
-                            MetaError::AmbiguousSource(sources)
-                        })
-                })?
-        };
-        Ok(Self {
-            current_dir,
-            root_dir,
-        })
-    }
 }
 
 #[cfg(test)]
---
+++ crates/mulan-config/src/meta.rs
@@ -0,0 +1,86 @@
+//! ...
+
+use std::ffi::OsStr;
+use std::path::PathBuf;
+use std::{env, io};
+
+use figment2::Figment;
+use itertools::Itertools as _;
+use mitsein::btree_set1::BTreeSet1;
+use mitsein::iter1::IteratorExt as _;
+use relative_path::RelativePathBuf;
+
+/// See [`Config::meta`].
+#[derive(Debug, Default, PartialEq, Eq)]
+pub struct ConfigMeta {
+    /// See [`std::env::current_dir`].
+    pub current_dir: PathBuf,
+
+    /// The path of the project root directory Mulan is operating on,
+    /// relative to [`Self::current_dir`].
+    pub root_dir: RelativePathBuf,
+}
+
+/// ...
+#[derive(Debug)]
+pub enum MetaError {
+    /// Failed to call [`std::env::current_dir`].
+    CurrentDir(io::Error),
+
+    /// Unable to locate a config file anywhere.
+    SourceNotFound,
+
+    /// Multiple config files found (only one is permitted).
+    AmbiguousSource(BTreeSet1<RelativePathBuf>),
+}
+
+impl ConfigMeta {
+    pub(super) fn new(figment: &Figment) -> Result<Self, MetaError> {
+        let current_dir = env::current_dir().map_err(MetaError::CurrentDir)?;
+        let (root_dir, _config_file) = {
+            figment
+                .metadata()
+                .filter_map(|metadata| {
+                    let source_absolute = {
+                        metadata
+                            .source
+                            .as_ref()
+                            .expect("all sources are predetermined")
+                            .file_path()
+                            .expect("config is only read from a file")
+                    };
+                    if source_absolute.is_relative() {
+                        return None;
+                    }
+                    let (root_dir_absolute, config_file) = {
+                        source_absolute
+                            .parent()
+                            .zip(source_absolute.file_name().and_then(OsStr::to_str))
+                            .expect("config source should point to a file")
+                    };
+                    let root_dir_raw = pathdiff::diff_paths(root_dir_absolute, &current_dir)
+                        .expect("current_dir can be subtracted from config source");
+                    let root_dir = RelativePathBuf::from_path(root_dir_raw)
+                        .expect("pathdiff::diff_paths returns a relative path");
+                    Some((root_dir, config_file))
+                })
+                .exactly_one()
+                .map_err(|locations| {
+                    locations
+                        .try_into_iter1()
+                        .map_or(MetaError::SourceNotFound, |sources_raw| {
+                            let sources = {
+                                sources_raw
+                                    .map(|(root_dir, config_file)| root_dir.join(config_file))
+                                    .collect1()
+                            };
+                            MetaError::AmbiguousSource(sources)
+                        })
+                })?
+        };
+        Ok(Self {
+            current_dir,
+            root_dir,
+        })
+    }
+}
