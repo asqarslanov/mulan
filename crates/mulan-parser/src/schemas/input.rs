bfe79150a9cae2b317f2dd078a71c35d2b9e3309
Asqar Arslanov <asqarslanov@gmail.com>
2026-08-01 00:42:59 +0500

    .

--- crates/mulan-parser/src/schemas/input.rs
+++ crates/mulan-parser/src/schemas/input.rs
@@ -120,7 +120,7 @@ impl Input {
 
 /// Errors of [`Definition::at`].
 #[derive(Debug, PartialEq, Eq)]
-pub(crate) enum DefinitionAtError {
+pub enum DefinitionAtError {
     /// The path doesn't exist.
     NotFound {
         /// The index (0-based) of the first subkey we couldn't enter/find.
