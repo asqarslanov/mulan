cae3fc70942bdb2a440295a9190d11955b769774
Asqar Arslanov <asqarslanov@gmail.com>
2026-08-22 22:03:41 +0500

    .

--- crates/mulan-gen/src/errors.rs
+++ crates/mulan-gen/src/errors.rs
@@ -2,9 +2,15 @@
 
 use std::io;
 
+/// Errors of [`crate::write_files`].
 #[derive(Debug)]
 pub enum GenError {
+    /// ...
     NoTargets,
+
+    /// ...
     CreateDir(io::Error),
+
+    /// ...
     WriteFile(io::Error),
 }
