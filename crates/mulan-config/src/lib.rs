7c9528d015707f077a46b55e6968e768664fb3cc
Asqar Arslanov <asqarslanov@gmail.com>
2026-08-22 22:51:14 +0500

    .

--- crates/mulan-config/src/lib.rs
+++ crates/mulan-config/src/lib.rs
@@ -58,7 +58,8 @@ pub struct Config {
     /// in another locale.
     pub main_locale: Language,
 
-    /// The list of targets for which i18n bindings should be generated.
+    /// The list of targets (i.e., programming languages) for which
+    /// i18n bindings should be generated.
     pub generate: Option<SmallVec1<[Target; 1]>>,
 
     /// Your preferred convention to name keys in locale definitions.
