638fc044925c6280d318ed1f3889c02bf05cc561
Asqar Arslanov <asqarslanov@gmail.com>
2026-08-23 23:09:14 +0500

    .

--- crates/mulan-cli/src/cmd_init.rs
+++ crates/mulan-cli/src/cmd_init.rs
@@ -1,7 +1,10 @@
 use std::io;
+use std::path::PathBuf;
 use std::process::ExitCode;
 
+use itertools::Itertools as _;
 use mulan_config::errors::{LocateError, NotFoundError};
+use mulan_config::{Language, RustTarget, Target};
 use relative_path::RelativePathBuf;
 
 use crate::error_reporting::ToReport as _;
@@ -22,11 +25,21 @@ pub fn execute() -> miette::Result<ExitCode> {
         Err(LocateError::Io(e)) => return Err(e.to_report(&mulan_config::Config::dummy())),
         Err(LocateError::NotFound(NotFoundError)) => (),
     }
-    match interactive_prompt() {
-        Ok(()) => Ok(ExitCode::SUCCESS),
-        Err(err) if matches!(err.kind(), io::ErrorKind::Interrupted) => Ok(ExitCode::from(SIGINT)),
-        Err(err) => panic!("{err}"),
-    }
+    let user_choice = match UserChoice::interactive_prompt() {
+        Ok(data) => data,
+        Err(err) if matches!(err.kind(), io::ErrorKind::Interrupted) => {
+            // Ctrl+C interrupt.
+            return Ok(ExitCode::from(SIGINT));
+        }
+        Err(err) => {
+            // A `cliclack` error indicates that we couldn't print to the console.
+            // There's no meaningful recovery strategy, so we just `panic!`.
+            // There's no point in creating rich Miette reports---
+            // we won't probably be able to print them anyway.
+            panic!("{err}");
+        }
+    };
+    todo!("create mulan.toml and locales/");
 }
 
 ///
@@ -37,9 +50,65 @@ pub struct ConfigExistsError {
 }
 
 ///
-fn interactive_prompt() -> io::Result<()> {
-    cliclack::intro("Mulan")?;
-    let _: String = cliclack::input("test").interact()?;
-    cliclack::outro("You're all set")?;
-    Ok(())
+#[derive(Debug)]
+struct UserChoice {
+    ///
+    locales: Vec<Language>,
+
+    ///
+    main_locale: Language,
+
+    ///
+    generate: Vec<Target>,
+}
+
+impl UserChoice {
+    ///
+    fn interactive_prompt() -> io::Result<Self> {
+        cliclack::intro("Mulan")?;
+        let locales = Self::prompt_locales(&[Language::EnUs, Language::RuRu])?;
+        let main_locale = *{
+            cliclack::select("main locale")
+                .items(
+                    &locales
+                        .iter()
+                        .map(|lang| (lang, lang.tag(), lang.name()))
+                        .collect_vec(),
+                )
+                .interact()?
+        };
+        let mut generate = Vec::new();
+        loop {
+            let add = cliclack::select("add a generation target")
+                .item(false, "no", "don't")
+                .item(true, "yes", "add")
+                .interact()?;
+            if !add {
+                break;
+            }
+            let path: PathBuf = cliclack::input("path")
+                .default_input("src/mulan.rs")
+                .interact()?;
+            generate.push(Target::Rust(RustTarget {
+                file: RelativePathBuf::from_path(path).unwrap(),
+            }));
+        }
+        cliclack::outro("You're all set")?;
+        Ok(UserChoice {
+            locales,
+            main_locale,
+            generate,
+        })
+    }
+
+    fn prompt_locales(all_possible_locales: &[Language]) -> io::Result<Vec<Language>> {
+        cliclack::multiselect("locales")
+            .items(
+                &all_possible_locales
+                    .iter()
+                    .map(|&lang| (lang, lang.tag(), lang.name()))
+                    .collect_vec(),
+            )
+            .interact()
+    }
 }
