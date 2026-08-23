852e03bce099893a1d48a291a8e484588d801fe9
Asqar Arslanov <asqarslanov@gmail.com>
2026-08-23 13:11:59 +0500

    .

--- crates/mulan-cli/src/cmd_init.rs
+++ crates/mulan-cli/src/cmd_init.rs
@@ -5,6 +5,16 @@ use std::process::ExitCode;
 /// ```sh
 /// $ mulan init ...
 /// ```
+#[expect(
+    clippy::unwrap_used,
+    reason = "
+        A `cliclack` error indicates that we can't print to the console.
+        There's no meaningful strategy to recover from this.
+        Also, there's no point in creating Miette reports---
+        we won't probably be able to print them anyway.
+    "
+)]
 pub fn execute() -> miette::Result<ExitCode> {
+    cliclack::intro("Hello").unwrap();
     todo!();
 }
