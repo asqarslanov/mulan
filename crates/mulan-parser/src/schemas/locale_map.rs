8fb9743c653c32e16818f47bbe458aeafb795af1
Asqar Arslanov <asqarslanov@gmail.com>
2026-08-28 01:25:53 +0500

    .

--- crates/mulan-parser/src/schemas/locale_map.rs
+++ crates/mulan-parser/src/schemas/locale_map.rs
@@ -1,1 +1,37 @@
+//! Defines the [`LocaleMap`].
 
+use foldhash::HashMap;
+use mulan_config::Language;
+
+use crate::{Identifier, Template};
+
+///
+#[derive(Debug)]
+pub struct LocaleMap {
+    ///
+    locales: HashMap<Language, LmDefinition>,
+}
+
+///
+#[derive(Debug)]
+struct LmDefinition {
+    ///
+    root: LmNamespace,
+}
+
+///
+#[derive(Debug)]
+struct LmNamespace {
+    ///
+    map: HashMap<Identifier, LmNode>,
+}
+
+///
+#[derive(Debug)]
+enum LmNode {
+    ///
+    Message(Template),
+
+    ///
+    Namespace(LmNamespace),
+}
